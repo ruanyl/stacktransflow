c3eb5e6ee427eebeab03f79e22453f331672d0e0
ruanyl <ruanyu1@gmail.com>
2016-03-21 22:15:44 +0200

    add toggle button

--- extension/content.js
+++ extension/content.js
@@ -18,5 +18,37 @@ function allAnswers() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(allAnswers());
+  const answerIds = allAnswers();
+
+  answerIds.forEach(function(answerId) {
+    let toggleBtn = [
+      '<p class="stf-trans-toggle">',
+      '<label class="stf-toggle-switch nolabel" data-answer-id="' + answerId + '" onclick="">',
+      '<input type="checkbox" />',
+      '<span>',
+      '<span>中文</span>',
+      '<span>中文</span>',
+      '</span>',
+      '<a></a>',
+      '</label>',
+      '</p>'
+    ].join('');
+    $('#answer-' + answerId + ' .answercell').prepend(toggleBtn);
+  });
+
+  $('.stf-toggle-switch').on('click', function() {
+    let toggleInput = $(this).find('input');
+    let answerId = $(this).attr('data-answer-id');
+    let checked = !toggleInput.prop('checked');
+    toggleInput.prop('checked', checked);
+
+    let postText = $('#answer-' + answerId + ' .answercell .post-text');
+    if(checked) {
+      if(!postText.find('.stf-answer').length) {
+        postText.prepend('<div class="stf-answer"></div>');
+      }
+    } else {
+      postText.find('.stf-answer').remove();
+    }
+  });
 });
